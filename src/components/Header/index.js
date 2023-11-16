ef9ba0aa1772875fc652751c05083924058a3ee7
Hailey Strauss <hstrauss2001@gmail.com>
2023-11-16 18:47:41 -0500

    add

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../Navigation/Navbar";
+
+export default function Header(props) {
+  return (
+    <div>
+      <header>
+        <Navigation />
+      </header>
+      <main></main>
+    </div>
+  );
+}
